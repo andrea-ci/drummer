d303b0afa215e07173c646330efad8b6640a9331
ndr3a <andrea.capitanelli@gmail.com>
2018-10-28 18:20:56 +0100

    fixed shebang

--- core/database/orm/schedule.py
+++ core/database/orm/schedule.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
 from core.database import SessionHandler
--- core/database/orm/worklog.py
+++ core/database/orm/worklog.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from sqlalchemy import Column, Integer, String, ForeignKey
 from core.database import SessionHandler
